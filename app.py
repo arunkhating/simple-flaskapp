06e5bc099aed135663750cadd54a61c1408530f0
arunkhating <140695960+arunkhating@users.noreply.github.com>
2025-03-11 15:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask is running in a container!"
+    return "Hello, Flask is running in a container! \
+    This is for Testing the Flask Container \
+    "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
